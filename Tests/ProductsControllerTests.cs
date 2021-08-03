349904c4e6df350feccebaad6842daab936f4a92
Pravin <khabilepravin@gmail.com>
2021-08-03 22:05:29 +1000

    Initial push for the CI

---
+++ Tests/ProductsControllerTests.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+using Moq;
+
+namespace Tests
+{
+    public class ProductsControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
---
+++ eXercise/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eXercise.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController()
+        {
+
+        }
+    }
+}
